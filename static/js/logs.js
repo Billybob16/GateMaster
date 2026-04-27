a6dd50d795ffade1339e9a0c7c0967e2d60dc222
Billybob16 <104896862+Billybob16@users.noreply.github.com>
2026-04-28 02:21:00 +1000

    Fix JavaScript syntax errors - add missing string quotes

--- static/js/logs.js
+++ static/js/logs.js
@@ -1,16 +1,11 @@
-﻿function loadLogs() {
+function loadLogs() {
     fetch("/api/logs")
         .then(r => r.json())
         .then(data => {
             let t = document.getElementById("logTable");
             t.innerHTML = "<tr><th>Time</th><th>Sender</th><th>Message</th></tr>";
             data.forEach(l => {
-                t.innerHTML += 
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>;
+                t.innerHTML += `<tr><td>${l.timestamp}</td><td>${l.sender}</td><td>${l.message}</td></tr>`;
             });
         });
 }
