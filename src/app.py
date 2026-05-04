98dba76c9d4782f4a309e2fbb16117febb2ec03e
Billybob16 <104896862+Billybob16@users.noreply.github.com>
2026-05-04 23:25:47 +1000

    Fix app.py to use templates folder correctly

--- src/app.py
+++ src/app.py
@@ -21,7 +21,7 @@ MOBILEMESSAGE_API_KEY = "<6tCVC8GFN4XPIQRJbrcAd1sXyR6WD4gVLxMjqqqYiU8>"
 # -------------------------------------------------
 # FLASK APP + DATABASE
 # -------------------------------------------------
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../templates')
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///rtu_config.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
